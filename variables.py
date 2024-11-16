ef6519ba3bb8c63a16b519c856ad4a0e92d50c98
230379DF <zinovjev.iwan2011@gmail.com>
2024-11-16 16:08:54 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+number_of_complited_homeworks = 12
+number_of_hours_spend = 1.5
+name_of_course = 'Python'
+time_for_one_task = number_of_hours_spend / number_of_complited_homeworks
+print('Курс:', name_of_course, ',всего задач:', number_of_complited_homeworks, ',затрачено часов:', number_of_hours_spend, ',среднее врямя выполнения:', time_for_one_task,'часов')
