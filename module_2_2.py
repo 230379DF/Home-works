fa8531fb13f010802b61ff3fac8efca4db52dc61
230379DF <zinovjev.iwan2011@gmail.com>
2024-11-22 03:21:54 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,14 @@
+number_1 = (12)
+number_2 = (13)
+number_3 = (11)
+if number_1 == number_2 and number_1 == number_3 and number_2 == number_3:
+    print('3')
+elif number_1 == number_2 or number_1 == number_3:
+    print('2')
+elif number_2 == number_3 or number_2 == number_1:
+    print('2')
+elif number_3 == number_1 or number_3 == number_2:
+    print('2')
+else: print('0')
+#elif number_1 != number_2 and number_1 != number_3 and number_2 != number_3:
+#    print('0')
