9e83160cfb674a45986f26704e0e593f2e46bdb1
230379DF <zinovjev.iwan2011@gmail.com>
2024-12-05 09:22:58 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -6,27 +6,23 @@ def count_calls():
 
 def string_info(string):
     count_calls()
-    list_ = []
-    list_.append(len(string))
     a = string.upper()
-    list_.append(a)
     b = string.lower()
-    list_.append(b)
-    tuple_ = tuple(list_)
+    tuple_ = (len(string), a, b)
     return tuple_
 
 def is_contains(string, list_to_search):
     count_calls()
-    a = string.lower()
-    b = str(list_to_search).lower()
-    if a in b:
-        print(True)
-    else:
-        print(False)
+    for x in list_to_search:
+        list_to_search.remove(x)
+        list_to_search.insert(0, x.lower())
+        if string.lower() in list_to_search:
+            print(string.lower() in list_to_search)
+            break
+    else: print(False)
 
 print(string_info('Авокадо'))
 print(string_info('Экзаменатор'))
-is_contains('око', ['ЯбЛоНя', 'оКНо', 'ЯБЛОКО'])
+is_contains('окно', ['ЯбЛоНя', 'оКНо', 'ЯБЛОКО'])
 is_contains('ГраД', ['ГаРдА', 'гРоЗа', 'ДУЭЛЬ'])
 print(calls)
-
